d39133294cd409ca39a2a38f1c72ac2fb9886b88
Aybe <aybe@users.noreply.github.com>
2017-12-20 05:54:09 +0100

    Cmake stuff

---
+++ build.bat
@@ -0,0 +1,11 @@
+msbuild vs17x86\libKeyFinder.vcxproj /t:rebuild /p:Configuration=Release
+msbuild vs17x86\libKeyFinder.vcxproj /t:rebuild /p:Configuration=RelWithDebInfo
+
+msbuild vs17x64\libKeyFinder.vcxproj /t:rebuild /p:Configuration=Release
+msbuild vs17x64\libKeyFinder.vcxproj /t:rebuild /p:Configuration=RelWithDebInfo
+
+msbuild vs17x86tc\libKeyFinder.vcxproj /t:rebuild /p:Configuration=Release
+msbuild vs17x86tc\libKeyFinder.vcxproj /t:rebuild /p:Configuration=RelWithDebInfo
+
+msbuild vs17x64tc\libKeyFinder.vcxproj /t:rebuild /p:Configuration=Release
+msbuild vs17x64tc\libKeyFinder.vcxproj /t:rebuild /p:Configuration=RelWithDebInfo
